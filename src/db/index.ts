0222a6ed8f1706ede2f04208650db4d3943c0e82
= <=>
2025-08-08 00:24:18 +0500

    Add: local Postgresql config using Podman Container

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,23 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+// Using NEON
+// import { neon } from "@neondatabase/serverless";
+// import { drizzle } from "drizzle-orm/neon-http";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL environment variable is not set");
-}
+// if (!process.env.DATABASE_URL) {
+//   throw new Error("DATABASE_URL environment variable is not set");
+// }
 
-const sql = neon(process.env.DATABASE_URL);
-export const db = drizzle({ client: sql });
+// const sql = neon(process.env.DATABASE_URL);
+// export const db = drizzle({ client: sql });
+
+//
+
+// Using PostgreSQL on Podman/Docker
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+});
+const db = drizzle({ client: pool });
+export { db };
