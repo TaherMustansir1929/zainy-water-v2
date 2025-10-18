a0a0b47d9440699c2bd674a0e782ab8554da8fd5
TaherMustansir1929 <eren.jeager1929101@gmail.com>
2025-10-18 22:16:48 +0500

    Update footer with AK Neotech

--- src/components/main-footer.tsx
+++ src/components/main-footer.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const MainFooter = ({ className }: Props) => {
   return (
     <footer className={cn("", className)}>
-      Neotech &trade; &bull; All Rights Reserved &copy; 2025
+      <sup>AK</sup>Neotech&trade; &bull; All Rights Reserved &copy; 2025
     </footer>
   );
 };
